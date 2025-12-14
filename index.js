fe467565bd56e36af76aea397c2256f50011e5f3
Blaq-boy101 <blaqskyper@gmail.com>
2025-12-14 19:37:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
+});
+io.on('connection', (socket) => {
+  console.log('a user connected');
 });
 
 server.listen(3000, () => {
@@ -36,5 +41,9 @@ server.listen(3000, () => {
     <form id="form" action="">
       <input id="input" autocomplete="off" /><button>Send</button>
     </form>
+      <script src="/socket.io/socket.io.js"></script>
+<script>
+  const socket = io();
+</script>
   </body>
 </html>
